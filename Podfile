e4e93ed3f14b6cb9c178a97fb646af42ae35bd80
Aleksey Get <=>
2019-12-12 14:40:45 +0800

    Setup workspace
    Create XCode project
    Setup SwiftLint
    Setup Cocoapods

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.0'
+
+target 'RubegProtocol' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RubegProtocol
+  pod 'BlueSocket', '~> 1.0.46'
+
+  target 'RubegProtocolTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RubegProtocol/RubegProtocol.h
@@ -0,0 +1,19 @@
+//
+//  RubegProtocol.h
+//  RubegProtocol
+//
+//  Created by Rubeg NPO on 12/12/2019.
+//  Copyright © 2019 Rubeg NPO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RubegProtocol.
+FOUNDATION_EXPORT double RubegProtocolVersionNumber;
+
+//! Project version string for RubegProtocol.
+FOUNDATION_EXPORT const unsigned char RubegProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RubegProtocol/PublicHeader.h>
+
+
