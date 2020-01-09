74789f04125fdb6f58a48ae62b4ad8335baac5f2
Aleksey Get <=>
2020-01-09 10:51:13 +0800

    Create custom error type for bytes-integers conversions

---
+++ RubegProtocol/ConversionError.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionError.swift
+//  RubegProtocol
+//
+//  Created by Rubeg NPO on 13/12/2019.
+//  Copyright © 2019 Rubeg NPO. All rights reserved.
+//
+
+import Foundation
+
+enum ConversionError: Error {
+    case wrongAmountOfBytes
+}
