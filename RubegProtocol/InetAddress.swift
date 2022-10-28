4904e41e85cbecfaed2dcfa4f8f06e09dbc5e188
Aleksey Get <=>
2022-10-28 15:55:49 +0800

    Allow to create a list of addresses of type InetAddress from a list of hostnames

--- RubegProtocol/InetAddress.swift
+++ RubegProtocol/InetAddress.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Network
 
 // swiftlint:disable identifier_name
 public class InetAddress {
@@ -20,19 +21,80 @@ public class InetAddress {
         return InetAddress(ip: ip, port: port)
     }
 
-    private static func validate(ip: String) throws {
-        let pattern = "((1\\d{1,2}|25[0-5]|2[0-4]\\d|\\d{1,2})\\.){3}(1\\d{1,2}|25[0-5]|2[0-4]\\d|\\d{1,2})"
-        let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+    public static func createAll(hosts: [String], port: Int32) -> [InetAddress] {
+        if !isValidPort(port) {
+            return []
+        }
 
-        if !predicate.evaluate(with: ip) {
+        return hosts.flatMap { host in
+            if isIpAddress(hostname: host) {
+                if isValidIp(host) {
+                    return [InetAddress(ip: host, port: port)]
+                } else {
+                    return []
+                }
+            }
+
+            return resolveDomainName(hostname: host)
+                .filter { ip in isValidIp(ip) }
+                .map { ip in InetAddress(ip: ip, port: port) }
+        }
+    }
+
+    private static func isIpAddress(hostname: String) -> Bool {
+        return hostname.allSatisfy { char in char.isNumber || char == "." }
+    }
+
+    private static func resolveDomainName(hostname: String) -> [String] {
+        var ips = [String]()
+
+        guard let host = (hostname.withCString { gethostbyname($0) }) else {
+            return ips
+        }
+
+        guard host.pointee.h_length > 0 else {
+            return ips
+        }
+
+        var index = 0
+        while host.pointee.h_addr_list[index] != nil {
+            var addr = in_addr()
+
+            memcpy(&addr.s_addr, host.pointee.h_addr_list[index], Int(host.pointee.h_length))
+
+            guard let remoteIPAsC = inet_ntoa(addr) else {
+                return ips
+            }
+
+            ips.append(String.init(cString: remoteIPAsC))
+
+            index += 1
+        }
+
+        return ips
+    }
+
+    private static func validate(ip: String) throws {
+        if !isValidIp(ip) {
             throw fatalError("Invalid ip")
         }
     }
 
     private static func validate(port: Int32) throws {
-        if port < 0 || port > 65535 {
+        if !isValidPort(port) {
             throw fatalError("Invalid port")
         }
+    }
+
+    private static func isValidIp(_ ip: String) -> Bool {
+        let octetPattern = "(1\\d{1,2}|25[0-5]|2[0-4]\\d|\\d{1,2})"
+        let pattern = "(\(octetPattern)\\.){3}\(octetPattern)"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        return predicate.evaluate(with: ip)
+    }
+
+    private static func isValidPort(_ port: Int32) -> Bool {
+        return port >= 0 && port <= 65535
     }
 
     private init(ip: String, port: Int32) {
